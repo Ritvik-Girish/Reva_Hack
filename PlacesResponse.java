013753b714a909b05c5695a662c91b4a8d58e367
Ritvik Girish <94665657+Ritvik-Girish@users.noreply.github.com>
2024-08-26 12:06:29 +0530

    Create PlacesResponse.java

---
+++ PlacesResponse.java
@@ -0,0 +1,11 @@
+package com.example.hospitallocator;
+
+import java.util.List;
+
+public class PlacesResponse {
+    private List<Place> results;
+
+    public List<Place> getResults() {
+        return results;
+    }
+}
